75ff1b09edcecdd2a2b4168768b70de7fb350366
Jeampierbq <Jeampierbarrios04@gmail.com>
2024-12-11 17:30:39 -0500

    avance Notas en Columna

---
+++ app/src/main/java/com/example/uc4_barrios/NotasInteractionListener.java
@@ -0,0 +1,7 @@
+package com.example.uc4_barrios;
+
+public interface NotasInteractionListener {
+    void editNotaClick(Nota nota);
+    void eliminaNotaClick (Nota nota);
+    void favoritaNotaClick (Nota nota);
+}
